b9d7d91ec2207cd817d33ef4dd9950a76eb42aa5
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2025-03-19 17:13:02 +0100

    age

--- conflict.js
+++ conflict.js
@@ -1,9 +1,12 @@
 function hello() {
   const name = 'Titi';
+  const age = 42;
 
   if (name) {
     console.log('Hello, ' + name);
   }
+
+  console.log('age: ' + age);
 
   return 'Hello World';
 }
