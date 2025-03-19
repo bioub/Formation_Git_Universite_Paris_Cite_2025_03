f161d7f947067b7dffbf8a98f65446c4ac599d50
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2025-03-19 17:14:32 +0100

    feat: job

--- conflict.js
+++ conflict.js
@@ -1,8 +1,10 @@
 function hello() {
   const name = 'Titi';
+  const job = 'Developer';
 
   if (name) {
     console.log('Hello, ' + name);
+    console.log('You are a ' + job);
   }
 
   return 'Hello World';
